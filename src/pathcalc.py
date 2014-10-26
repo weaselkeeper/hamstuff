ce6dd9c1f26f74ce4238b9b38689afd999a14bd3
Jim Richardson <weaselkeeper@gmail.com>
2014-10-26 13:32:17 -0700

    rewrite of pathcalc.py using tkinter, and some cleanup

--- src/pathcalc.py
+++ src/pathcalc.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env  python
+#!/usr/bin/env	python
 #
 #
-# Copyright (C) 2014 Jim Richardson
-# email	weaselkeeper@gmail.com
+#Copyright (C) 2000 Jim Richardson
+#email	weaselkeeper@gmail.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,26 +17,28 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-# 	02111-1307, USA.
+#	02111-1307, USA.
 #
-# This program is a simple calculator for some basic parabolic
-# antenna equations. It's crude, but at least seems bug free. It
-# makes a few assumptions, first that the numbers are all related
-# to free space, so any atmospheric losses, including rain fade
-# must be calculated seperately (I may add this at a future time)
-# Second, it assumes that you know not to use dumb combinations of
-# numbers, like a parabolic dish of 10cm, with a freq of 100Mhz.
-# The numbers generated in such a combo are mathematically
-# correct, and in practical terms, bogus.
-# I also assume that both antennas are the same diameter, this
-# will change in future revisions.
+#This program is a simple calculator for some basic parabolic
+#antenna equations. It's crude, but at least seems bug free. It
+#makes a few assumptions, first that the numbers are all related
+#to free space, so any atmospheric losses, including rain fade
+#must be calculated seperately (I may add this at a future time)
+#Second, it assumes that you know not to use dumb combinations of
+#numbers, like a parabolic dish of 10cm, with a freq of 100Mhz.
+#The numbers generated in such a combo are mathematically
+#correct, and in practical terms, bogus.
+#I also assume that both antennas are the same diameter, this
+#will change in future revisions.
 #
-# Most of the calculations come from a GTE microwave journal
-# that is over 30 years old :) Information is forever.
+#Most of the calculations come from a GTE microwave journal
+#that is over 30 years old :) Information is forever.
 #
-# Comments, bug notices, feature lists are welcome at
-# weaselkeeper@gmail.com, please direct flames to /dev/null, I will...
+#Comments, bug notices, feature lists are welcome at
+#weaselkeeper@gmail.com, please direct flames to /dev/null, I will...
 
+
+# Reimplementation of pathcalc in python+tkinter
 
 """
 
@@ -44,85 +46,150 @@ License: GPL V2 See LICENSE file
 Author: Jim Richardson
 email: weaselkeeper@gmail.com
 
-A rewrite of pathcalc.tcl in Python
-
 """
+
 PROJECTNAME = 'pathcalc'
 
-
-# Do the import stuff.
-import os
 import sys
+import os
 import ConfigParser
+import Tkinter as Tk
 import logging
-
-
-# Setup logging.
-
+import math
+# Setup logging
 logging.basicConfig(level=logging.WARN,
                     format='%(asctime)s %(levelname)s - %(message)s',
                     datefmt='%y.%m.%d %H:%M:%S')
 
 # Setup logging to console.
-CONSOLE = logging.StreamHandler(sys.stderr)
-CONSOLE.setLevel(logging.WARN)
-logging.getLogger(PROJECTNAME).addHandler(CONSOLE)
-LOG = logging.getLogger(PROJECTNAME)
+console = logging.StreamHandler(sys.stderr)
+console.setLevel(logging.WARN)
+logging.getLogger(PROJECTNAME).addHandler(console)
+log = logging.getLogger(PROJECTNAME)
+
+
+# set up some default settings, use if no config presented.
+defaults = {'dia': (0.1, 1), 'freq': (0.1, 10), 'path': (0.05, 10.0), 'lambda': (0, 50)}
+print defaults
+
+
+#set upper and lower limits for freq, path length etc
+#
+
+class PathCalc(object):
+    """ Instantiate a path calc object """
+
+    def __init__(self):
+        """ Set some basic starting limits """
+        self = read_config(self)
+        self.title = 'Parapath Calculator'
+
+    def run(self):
+        """ Execute the run method """
+        log.debug('In run()')
+        # Draw the canvas
+        root = Tk.Tk()
+        var = Tk.StringVar()
+        var.set(self.title)
+        Tk.Label(root, textvariable=var).pack()
+        for variable in self.settings:
+            _min, _max = getattr(self, variable).split(',')
+            slider = Tk.Scale(root, label=variable, from_=_min,
+                              to=_max, resolution=0.1, orient='horizontal',
+                              length=250, command=variable)
+            slider.pack()
+
+        Tk.Button(root, text="Quit", command=root.quit).pack()
+        root.mainloop()
+
+    def change_freq(self):
+        """ when freq changes, recalculate all the stuff that changes as a
+        result"""
+        log.debug('in change_freq')
+        log.warn('now recalc pathloss, paragain, 3dbTheta and lambda')
+        self.paragain()
+        self.threedb_theta()
+        self.lambdaCalc()
+        self. pathloss()
+
+    def lambdaCalc(self):
+        """ Calculating lambda (wavelength) """
+        log.debug('in lambdaCalc')
+        self._lambda = 300.00/self.freq
+        print self._lambda
+
+    def threedb_theta(self):
+        """ Calculating the 3db theta point """
+        log.debug('In threedb_theta')
+        self.threedb_theta = 22.00/self.freq*self.para_dia
+
+    def paraGain(self):
+        """ Calculating the dish gain per side """
+        log.debug('in paraGain calculation')
+        self.para_gain = (20*math.log(10, self.dia)+(20*math.log(10, self.freq)+17.8))
+
+    def pathloss(self):
+        """ Calculate the full path loss """
+        self.path_loss = (92.4+20*math.log(10, self.freq)+20*math.log(10, self.path_length))
+
+    def pathgain(self):
+        """ Calc total path gain """
+        self.path_gain = self.path_loss + self.para_gain
 
 
 def get_options():
-    """ Parse the command line options"""
+    """ Parse for any options """
+    log.debug('in get_options')
+    # Default config file
     import argparse
-
     parser = argparse.ArgumentParser(
-        description='GUI path calculator for parabolic antenna pairs.')
+        description='This is a Parabolic dish path calculator.')
+    parser.add_argument('-f', '--file', action='store', default=None,
+        help='Input file', dest='inputfile')
+    parser.add_argument('-c', '--config', action='store', help='Config file')
     parser.add_argument('-d', '--debug', action='store_true',
-                        help='Enable debugging during execution.',
-                        default=None)
-    parser.add_argument('-c', '--config', action='store', default=None,
-                        help='Specify a path to an alternate config file')
+        help='enable debugging')
     _args = parser.parse_args()
     _args.usage = PROJECTNAME + ".py [options]"
 
-    if _args.debug:
-        LOG.setLevel(logging.DEBUG)
-    else:
-        LOG.setLevel(logging.WARN)
-
+    # If we specify a config, then we use it, if not, we go with supplied
+    # options
+    log.debug('leaving get_options')
     return _args
 
 
-def get_config(_args):
-    """ Now parse the config file.  Get any and all info from config file."""
-    LOG.debug('Now in get_config')
-    parser = ConfigParser.SafeConfigParser()
-    configuration = {}
+def read_config(_object):
+    """ We will now pass the config settings into the object """
+    log.debug('In read_config')
     configfile = os.path.join('/etc', PROJECTNAME, PROJECTNAME + '.conf')
-    if _args.config:
-        _config = _args.config
+    config = ConfigParser.SafeConfigParser()
+    if args.config:
+        _config = args.config
+        config.read(_config)
     else:
         if os.path.isfile(configfile):
-            _config = configfile
+            config.read(configfile)
         else:
-            LOG.warn('No config file found at %s', configfile)
+            log.warn('No config file found, continue with args passed')
             sys.exit(1)
 
-    parser.read(_config)
+    items = config.options('sliders')
+    _object.settings = items
+    for item in items:
+        value = config.get('sliders', item)
+        setattr(_object, item, value)
+    return _object
 
-    LOG.debug('Doing things with %s', configuration['SOURCEURL'])
-    LOG.debug('leaving get_config')
-    return configuration
-
-
-def run(_args):
-    """ Base run function """
-    print _args
-
-
-# Here we start if called directly (the usual case.)
-if __name__ == "__main__":
+if __name__ == '__main__':
     # This is where we will begin when called from CLI. No need for argparse
-    # unless being called interactively, so import it here
-    ARGS = get_options()
-    # and now we can do, whatever it is, we do.
-    sys.exit(run(ARGS))
+    # unless being called interactively, so import it here, or at least, in
+    # get_options
+    args = get_options()
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+    else:
+        log.setLevel(logging.WARN)
+
+    canvas = PathCalc()
+    canvas.run()
